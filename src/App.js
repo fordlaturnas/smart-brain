1c5160c2d4cc19da39f400906acaeb98780f3058
Ford Laturnas <fordlaturnas1209@gmail.com>
2022-02-03 17:02:38 +0800

    added the new endpoint

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl : this.state.input});
 
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://afternoon-dawn-04869.herokuapp.com//imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component {
       .then(result => {
         this.displayFaceBox(this.calculateFaceLocation(result));
         if (result) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://afternoon-dawn-04869.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://afternoon-dawn-04869.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
