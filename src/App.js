e95d4161549503cd6c79b4a492949ca399187163
Ford Laturnas <fordlaturnas1209@gmail.com>
2022-02-03 23:41:43 +0800

    fix bugs on onButtonSumit

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl : this.state.input});
 
-      fetch('https://afternoon-dawn-04869.herokuapp.com//imageurl', {
+      fetch('https://afternoon-dawn-04869.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
